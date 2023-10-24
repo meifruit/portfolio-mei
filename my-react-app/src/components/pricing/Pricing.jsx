1414a4957574d61bc1746286f676956df30c8b14
Xuemei Huang <mei.huang9500@gmail.com>
2023-10-24 20:03:43 +0900

    change folder

---
+++ my-react-app/src/components/pricing/Pricing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./pricing.css";
+const Pricing = () => {
+  return <div>Pricing</div>;
+};
+
+export default Pricing;
---
+++ my-react-app/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./sidebar.css";
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;

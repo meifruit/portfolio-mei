feb2c415ffc299e89423dc1d26a5ec9b7b82ff5b
Xuemei Huang <mei.huang9500@gmail.com>
2023-10-27 15:16:20 +0900

    portfolio component

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,50 @@
+import Work1 from "../../assets/work-1.svg";
+import Work2 from "../../assets/work-2.svg";
+import Work3 from "../../assets/work-3.svg";
+import Work4 from "../../assets/work-4.svg";
+import Work5 from "../../assets/work-5.svg";
+import Work6 from "../../assets/work-6.svg";
+
+const Menu = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Project Management Illustration",
+    category: "Design",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Guest App Walkthrough Screens",
+    category: "Personalwork",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Delivery App Wireframe",
+    category: "Ecsite",
+  },
+  ,
+  {
+    id: 4,
+    image: Work4,
+    title: "Onboarding Motivation",
+    category: "Design",
+  },
+  ,
+  {
+    id: 5,
+    image: Work5,
+    title: "iMac Mockup Design",
+    category: "Groupwork",
+  },
+  ,
+  {
+    id: 6,
+    image: Work6,
+    title: "Game Store App Concept",
+    category: "Art",
+  },
+];
+
+export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
+import Menu from "./Menu";
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  const [items, setItems] = useState(Menu);
+  const filterItem = (categoryItem) => {
+    const updatedItems = Menu.filter((curEl) => {
+      return curEl.category === categoryItem;
+    });
+    setItems(updatedItems);
+  };
+  return (
+    <section className="work container section" id="work">
+      <h2 className="section__title">Project</h2>
+      <div className="work__filters">
+        <span className="work__item" onClick={() => setItems(Menu)}>
+          Everything
+        </span>
+        <span className="work__item" onClick={() => filterItem("Groupwork")}>
+          Groupwork
+        </span>
+        <span className="work__item" onClick={() => filterItem("Personalwork")}>
+          PersonalWork
+        </span>
+        <span className="work__item" onClick={() => filterItem("Design")}>
+          Design
+        </span>
+        <span className="work__item" onClick={() => filterItem("Ecsite")}>
+          ECsite
+        </span>
+      </div>
+      <div className="work__container grid">
+        {items.map((elem) => {
+          const { id, image, title, category } = elem;
+          return (
+            <div className="work__card" key={id}>
+              <div className="work__thumbnail">
+                <img src={image} alt="" className="work__img" />
+                <div className="work__mask"></div>
+              </div>
+              <span className="work__category">{category}</span>
+              <h2 className="work__title">{title}</h2>
+              <a href="#" className="work__button">
+                <i className="icon-link work__butoon-icon"></i>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
